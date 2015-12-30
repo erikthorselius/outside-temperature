150482d02e47040c4e6e636909ec124f04c0b153
Erik Thorselius <erik.thorselius@gmail.com>
2015-12-30 08:03:24 +0100

    Change to PUB/SUB pattern

--- server.py
+++ server.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
-import zmq, json, sys, os 
+import zmq, json, sys, os
 from datetime import datetime, timedelta
 
 socket_address = os.getenv('OUTSIDE_SOCKET')
 context = zmq.Context()
-socket = context.socket(zmq.PULL)
+socket = context.socket(zmq.SUB)
 socket.bind(socket_address)
+socket.setsockopt_string(zmq.SUBSCRIBE, 'sensor')
 
-while True: 
-  result = socket.recv_json()
-  print(result)
-
+if __name__ == '__main__':
+    while True:
+        topic, message = socket.recv_multipart()
+        print("%s %s" % (topic, message))
--- temperature_sender.py
+++ temperature_sender.py
@@ -5,57 +5,63 @@ from datetime import datetime, timedelta
 api_url = os.getenv('OUTSIDE_API_URL')
 report_time = float(os.getenv('OUTSIDE_REPORT_TIME', 3600))
 socket_address = os.getenv('OUTSIDE_SOCKET').strip()
-logging.basicConfig(format='%(asctime)s %(message)s',level=logging.DEBUG)
+logging.basicConfig(format='%(asctime)s %(message)s', level=logging.DEBUG)
 
 context = zmq.Context()
-socket = context.socket(zmq.PUSH)
+socket = context.socket(zmq.PUB)
 socket.connect(socket_address)
-logging.info('Connected to address %s',socket_address)
+logging.info('Connected to address %s', socket_address)
 s = sched.scheduler(time.time, time.sleep)
 
+
 def signal_handler(signal, frame):
-  print('You pressed Ctrl+C! Shutting down')
-  socket.close(linger=1)
-  sys.exit(0)
+    print('You pressed Ctrl+C! Shutting down')
+    socket.close(linger=1)
+    sys.exit(0)
+
 
 signal.signal(signal.SIGTERM, signal_handler)
 signal.signal(signal.SIGINT, signal_handler)
 
-def send(sensor):
-  value = sensor.get_value()
-  logging.debug('Send value: %s to server',value)
-  socket.send_json(value) 
 
-def report_temperature_job(sc, sensor): 
-  send(sensor)
-  sc.enter(report_time, 1, report_temperature_job, (sc,sensor))
+def send(sensor):
+    value = sensor.get_value()
+    logging.debug('Send value: %s to server', value)
+    socket.send_multipart([b'sensor', bytes(json.dumps(value),'utf8')])
+
+
+def report_temperature_job(sc, sensor):
+    send(sensor)
+    sc.enter(report_time, 1, report_temperature_job, (sc, sensor))
+
 
 class TempSensor:
-  def __init__(self, name):
-      self.dict = {}
-      self.dict['name'] = name
-      self.dict['type'] = 'external_api'
-      self.dict['sensor'] = 'temperature'
+    def __init__(self, name):
+        self.dict = {}
+        self.dict['name'] = name
+        self.dict['type'] = 'external_api'
+        self.dict['sensor'] = 'temperature'
 
-  def __is_within_hour(self, item): 
-    date = datetime.strptime(item['validTime'], "%Y-%m-%dT%H:%M:%SZ")
-    return datetime.now() < date < (datetime.now() + timedelta(hours=1))
+    def __is_within_hour(self, item):
+        date = datetime.strptime(item['validTime'], "%Y-%m-%dT%H:%M:%SZ")
+        return datetime.now() < date < (datetime.now() + timedelta(hours=1))
 
-  def __get_data_from_api(self):
-    response = urllib.request.urlopen(api_url).read()
-    return json.loads(response.decode('utf-8'))
+    def __get_data_from_api(self):
+        response = urllib.request.urlopen(api_url).read()
+        return json.loads(response.decode('utf-8'))
 
-  def __get_nearest_item(self, forcast):
-    return next(item['t'] for item in forcast if self.__is_within_hour(item))
+    def __get_nearest_item(self, forcast):
+        return next(item['t'] for item in forcast if self.__is_within_hour(item))
 
-  def get_value(self):
-    try:  
-      forcast = self.__get_data_from_api()['timeseries']
-      self.dict['value'] = float(self.__get_nearest_item(forcast))
-      return self.dict
-    except IOError:
-      return {'error': 'Could not connect to API', 'name': self.dict['name']}
+    def get_value(self):
+        try:
+            forcast = self.__get_data_from_api()['timeseries']
+            self.dict['value'] = float(self.__get_nearest_item(forcast))
+            return self.dict
+        except IOError:
+            return {'error': 'Could not connect to API', 'name': self.dict['name']}
 
-s.enter(report_time, 1, report_temperature_job, (s,TempSensor('Outside')))
+
+s.enter(report_time, 1, report_temperature_job, (s, TempSensor('Outside')))
 logging.info('Add report jobb to scheduler, will run every %s second', report_time)
 s.run()
